8db21a23d4054860222736b5288b838f8d5b7167
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-07-14 21:54:23 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,26 @@ microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (message) {
     microIoT.microIoT_clear()
     microIoT.microIoT_showUserText(0, "Mode: " + Mode)
 })
+function curtainUp () {
+    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+    basic.pause(2000)
+    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
+}
+input.onButtonPressed(Button.A, function () {
+    curtainUp()
+    microIoT.microIoT_clear()
+    microIoT.microIoT_showUserText(0, "Curtain is moving up")
+})
+function curtainDown () {
+    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 180)
+    basic.pause(2000)
+    microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
+}
+input.onButtonPressed(Button.B, function () {
+    curtainDown()
+    microIoT.microIoT_clear()
+    microIoT.microIoT_showUserText(0, "Curtain is moving down")
+})
 let Mode = ""
 let wifi_name = "izowifi"
 let password = "izo1234@"
@@ -17,14 +37,3 @@ iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
-basic.forever(function () {
-    if (Mode == "sunny mode") {
-    	
-    } else if (Mode == "rainy mode") {
-    	
-    } else if (Mode == "sleeping mode") {
-    	
-    } else if (Mode == "security mode") {
-    	
-    }
-})
