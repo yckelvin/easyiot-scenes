d8433ab83a6e5b476d94f46245b0d37779ea2b6f
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:28:29 +0800

    add debug

--- main.ts
+++ main.ts
@@ -26,16 +26,20 @@ input.onButtonPressed(Button.B, function () {
     microIoT.microIoT_showUserText(1, "moving down")
 })
 let Mode = ""
+basic.showNumber(0)
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(1)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(2)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(3)
